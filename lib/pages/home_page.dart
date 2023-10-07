3b79106f1bec946e1719e7bdb9be061af755df63
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-10-07 22:36:49 +0200

    user review and some stuff

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
             ),
           ),
           child: Container(
-            padding: EdgeInsets.all(16),
+            padding: EdgeInsets.all(64),
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.bottomCenter,
@@ -33,7 +33,7 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-            child: Align(
+            child: Container(
               alignment: Alignment.bottomLeft,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
@@ -55,19 +55,69 @@ class HomePage extends StatelessWidget {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            "Destinations ready to EXPLORE:",
-            style: tt.headlineMedium,
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Destinations ready to EXPLORE:",
+                style: tt.headlineMedium,
+              ),
+              Row(
+                children: [
+                  RoundedDestinationPreview(),
+                  RoundedDestinationPreview(),
+                  RoundedDestinationPreview(),
+                ],
+              ),
+              Text(
+                "Reviews from our Travelers:",
+                style: tt.headlineMedium,
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 384,
+                    height: 256,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(color: c.onBackground.withAlpha(96)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 64,
+                                height: 64,
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    'assets/mars.jpg',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Max',
+                                style: tt.titleMedium,
+                              ),
+                            ],
+                          ),
+                          Divider(),
+                          Text(
+                              "I didn't expect that interplanetary travel could be so fascinating! Mars surprised me with it’s beautiful red scenery. The travel agency took care of every detail, and the scientific excursions were truly educational. I recommend it to anyone who dreams of a unique adventure beyond our planet!"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        Row(
-          children: [
-            RoundedDestinationPreview(),
-            RoundedDestinationPreview(),
-            RoundedDestinationPreview(),
-          ],
-        )
       ],
     );
   }
--- lib/theme.dart
+++ lib/theme.dart
@@ -12,27 +12,29 @@ final _c = _base.colorScheme;
 final _tt = _base.textTheme;
 final darkTheme = _base.copyWith(
   textTheme: _base.textTheme.copyWith(
-    displayLarge: _tt.displayLarge!.copyWith(
-      color: _c.primary,
-      fontSize: 192,
-      fontFamily: 'Ron',
-    ),
-    displayMedium: _tt.displayMedium!.copyWith(
-      color: _c.primary,
-      fontSize: 86,
-      fontFamily: 'Ron',
-    ),
-    headlineLarge: _tt.headlineLarge!.copyWith(
-      color: _c.primary,
-      fontSize: 32,
-    ),
-    headlineMedium: _tt.headlineMedium!.copyWith(
-      color: _c.primary,
-      fontSize: 24,
-    ),
-    headlineSmall: _tt.headlineSmall!.copyWith(
-      color: _c.primary,
-      fontSize: 16,
-    ),
-  ),
+      displayLarge: _tt.displayLarge!.copyWith(
+        color: _c.primary,
+        fontSize: 192,
+        fontFamily: 'Ron',
+      ),
+      displayMedium: _tt.displayMedium!.copyWith(
+        color: _c.primary,
+        fontSize: 86,
+        fontFamily: 'Ron',
+      ),
+      headlineLarge: _tt.headlineLarge!.copyWith(
+        color: _c.primary,
+        fontSize: 32,
+      ),
+      headlineMedium: _tt.headlineMedium!.copyWith(
+        color: _c.primary,
+        fontSize: 24,
+      ),
+      headlineSmall: _tt.headlineSmall!.copyWith(
+        color: _c.primary,
+        fontSize: 16,
+      ),
+      titleMedium: _tt.titleMedium!.copyWith(
+        fontSize: 20,
+      )),
 );
