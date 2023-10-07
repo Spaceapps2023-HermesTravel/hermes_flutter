d0f018fad8a32f1b92864388e9f67c2c958a1b56
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-10-07 23:14:57 +0200

    papaj

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,40 +1,45 @@
 import 'package:flutter/material.dart';
 
+final _zolty = Color(0xffedaa25);
 final _base = ThemeData(
   colorScheme: ColorScheme.fromSeed(
-    seedColor: Color(0x00edaa25),
+    seedColor: _zolty,
     brightness: Brightness.dark,
   ),
   useMaterial3: true,
 );
 
-final _c = _base.colorScheme;
+final _c = _base.colorScheme.copyWith(
+  primary: _zolty,
+);
 final _tt = _base.textTheme;
 final darkTheme = _base.copyWith(
+  colorScheme: _c,
   textTheme: _base.textTheme.copyWith(
-      displayLarge: _tt.displayLarge!.copyWith(
-        color: _c.primary,
-        fontSize: 192,
-        fontFamily: 'Ron',
-      ),
-      displayMedium: _tt.displayMedium!.copyWith(
-        color: _c.primary,
-        fontSize: 86,
-        fontFamily: 'Ron',
-      ),
-      headlineLarge: _tt.headlineLarge!.copyWith(
-        color: _c.primary,
-        fontSize: 32,
-      ),
-      headlineMedium: _tt.headlineMedium!.copyWith(
-        color: _c.primary,
-        fontSize: 24,
-      ),
-      headlineSmall: _tt.headlineSmall!.copyWith(
-        color: _c.primary,
-        fontSize: 16,
-      ),
-      titleMedium: _tt.titleMedium!.copyWith(
-        fontSize: 20,
-      )),
+    displayLarge: _tt.displayLarge!.copyWith(
+      color: _c.primary,
+      fontSize: 192,
+      fontFamily: 'Ron',
+    ),
+    displayMedium: _tt.displayMedium!.copyWith(
+      color: _c.primary,
+      fontSize: 86,
+      fontFamily: 'Ron',
+    ),
+    headlineLarge: _tt.headlineLarge!.copyWith(
+      color: _c.primary,
+      fontSize: 32,
+    ),
+    headlineMedium: _tt.headlineMedium!.copyWith(
+      color: _c.primary,
+      fontSize: 24,
+    ),
+    headlineSmall: _tt.headlineSmall!.copyWith(
+      color: _c.primary,
+      fontSize: 16,
+    ),
+    titleMedium: _tt.titleMedium!.copyWith(
+      fontSize: 20,
+    ),
+  ),
 );
