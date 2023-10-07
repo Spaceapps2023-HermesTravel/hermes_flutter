d747c4a3d094fb72a2c0a5ea50ddb690fe34f227
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-10-07 20:31:28 +0200

    initial home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,61 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final t = Theme.of(context);
+    final c = t.colorScheme;
+    final s = MediaQuery.of(context).size;
+    return Container(
+      height: s.height,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/home-bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        width: double.infinity,
+        height: 128,
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              c.background,
+              Colors.transparent,
+            ],
+          ),
+        ),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: AutoSizeText(
+                  'FROM OUTER SPACE TO OLIMPUS',
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: c.primary,
+                    fontSize: 32,
+                  ),
+                ),
+              ),
+              AutoSizeText(
+                'Hermes',
+                maxLines: 1,
+                style: TextStyle(
+                  color: c.primary,
+                  fontFamily: 'Ron',
+                  fontSize: 192,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
