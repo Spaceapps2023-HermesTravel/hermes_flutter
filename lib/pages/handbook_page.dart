f735d170953e8fabbb3c70e7a7b316324a795744
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-10-08 11:53:34 +0200

    handbook download

--- lib/pages/handbook_page.dart
+++ lib/pages/handbook_page.dart
@@ -1,10 +1,41 @@
+import 'package:file_saver/file_saver.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class HandbookPage extends StatelessWidget {
   const HandbookPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 128, horizontal: 32),
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset('assets/handbook.png'),
+          ),
+          SizedBox(height: 16),
+          FilledButton(
+            onPressed: () async {
+              FileSaver.instance.saveFile(
+                name: 'handbook.docx',
+                bytes: (await rootBundle.load('assets/handbook.docx'))
+                    .buffer
+                    .asUint8List(),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                'Download handbook',
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
